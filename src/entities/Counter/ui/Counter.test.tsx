0b9889c35d92600905f4c0a4fb8a88fd369a81eb
Евгений Романенко <stworkout83@gmail.com>
2023-05-27 13:16:17 +0300

    add redux store, add first entity

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,28 @@
+import { Sidebar } from 'widgets/Sidebar';
+import { screen } from '@testing-library/react';
+import { Counter } from 'entities/Counter';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import { userEvent } from '@storybook/testing-library';
+
+describe('Counter', () => {
+    test('test render', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+    }); test('increment', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        userEvent.click(screen.getByTestId('increment-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+    });
+    test('decrement', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        userEvent.click(screen.getByTestId('decrement-btn'));
+        expect(screen.getByTestId('value-title'))
+            .toHaveTextContent('9');
+    });
+});
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Counter } from 'entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation();
 
     return (
         <div>
+            <Counter />
             {t('Главная страница')}
+
         </div>
     );
 };
