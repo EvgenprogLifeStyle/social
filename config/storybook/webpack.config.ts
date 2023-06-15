8115fe478270e4464a16d213429e06a64e2fb463
Евгений Романенко <stworkout83@gmail.com>
2023-06-15 18:16:16 +0300

    update husky

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -13,7 +13,7 @@ export default ({ config }:{config:webpack.Configuration}) => {
     };
     config.resolve.modules.push(paths.src);
     config.resolve.extensions.push('.ts', '.tsx');
-    config.module.rules.push(buildSvgLoader());
+    // config.module.rules.push(buildSvgLoader());
     // eslint-disable-next-line no-param-reassign
     config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
@@ -23,6 +23,10 @@ export default ({ config }:{config:webpack.Configuration}) => {
         return rule;
     });
 
+    config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    });
     config.module.rules.push(buildCssLoader(true));
 
     config.plugins.push(new DefinePlugin({
