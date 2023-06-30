ca6bf39028f74ef968561deb3702c9f209473550
Евгений Романенко <stworkout83@gmail.com>
2023-06-30 08:41:32 +0300

    add testing asyncThunk

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -23,7 +23,6 @@ export const loginByUsername = createAsyncThunk<User, loginByUsernameProps, { re
 
             return response.data;
         } catch (e) {
-            console.log(e);
             return thunkAPI.rejectWithValue('error');
         }
     },
